d73b82cfaee280edd0b0b5f453b346273d5f59a8
kp4ws <kentpawson123@gmail.com>
2021-05-08 14:36:21 -0600

    Added project files

---
+++ Assets/TerrainGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+	[SerializeField] LevelChunk chunk;
+	[SerializeField] private int mapWidthInChunks;
+	[SerializeField] private int mapLengthInChunks;
+
+    void Update()
+    {
+		for (int z = 0; z < mapLengthInChunks; z++)
+		{
+			for (int x = 0; x < mapWidthInChunks; x++)
+			{
+
+			}
+		}
+    }
+}
